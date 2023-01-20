3a936f2e87af864fc1cd9da6c5b6f86e4e181482
Yaroslav Terekh <yarolslavterekh@gmail.com>
2023-01-20 15:57:32 +0200

    Added new property to user

---
+++ GameBlog/Migrations/20230120135630_AddAboutMePropertyToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAboutMePropertyToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AboutMe",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AboutMe",
+                table: "AspNetUsers");
+        }
+    }
+}
